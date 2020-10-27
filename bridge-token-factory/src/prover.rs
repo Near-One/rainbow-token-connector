b59f06ac2fc3437a9ef63668ac0ed6683a42f08f
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2020-10-27 17:55:54 -0400

    refactor: run 'cargo fmt' in all Rust directories
    [rustfmt] can be added to most editors, and can also be run using `cargo
    fmt` within a specific Rust project
    
      [rustfmt]: https://doc.rust-lang.org/book/appendix-04-useful-development-tools.html

--- bridge-token-factory/src/prover.rs
+++ bridge-token-factory/src/prover.rs
@@ -2,20 +2,19 @@ use std::convert::From;
 
 use borsh::{BorshDeserialize, BorshSerialize};
 use eth_types::*;
-use ethabi::{Event, EventParam, Hash, ParamType, RawLog, Token, Log};
 use ethabi::param_type::Writer;
+use ethabi::{Event, EventParam, Hash, Log, ParamType, RawLog, Token};
 //use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{ext_contract};
+use near_sdk::ext_contract;
 
 use tiny_keccak::Keccak;
 
 pub type EthAddress = [u8; 20];
 
 pub fn validate_eth_address(address: String) -> EthAddress {
-    let data =
-        hex::decode(address).expect("address should be a valid hex string.");
+    let data = hex::decode(address).expect("address should be a valid hex string.");
     assert_eq!(data.len(), 20, "address should be 20 bytes long");
-    let mut result  = [0u8; 20];
+    let mut result = [0u8; 20];
     result.copy_from_slice(&data);
     result
 }
@@ -54,7 +53,18 @@ pub struct EthEvent {
 
 impl EthEvent {
     pub fn from_log_entry_data(name: &str, params: EthEventParams, data: &[u8]) -> Self {
-        let event = Event { name: name.to_string(), inputs: params.into_iter().map(|(name, kind, indexed)| EventParam { name, kind, indexed }).collect(), anonymous: false };
+        let event = Event {
+            name: name.to_string(),
+            inputs: params
+                .into_iter()
+                .map(|(name, kind, indexed)| EventParam {
+                    name,
+                    kind,
+                    indexed,
+                })
+                .collect(),
+            anonymous: false,
+        };
         let log_entry: LogEntry = rlp::decode(data).expect("Invalid RLP");
         let locker_address = (log_entry.address.clone().0).0;
         let raw_log = RawLog {
@@ -68,18 +78,34 @@ impl EthEvent {
         let log = event.parse_log(raw_log).expect("Failed to parse event log");
         Self {
             locker_address,
-            log
+            log,
         }
     }
 
-    pub fn to_log_entry_data(name: &str, params: EthEventParams, locker_address: EthAddress, indexes: Vec<Vec<u8>>, values: Vec<Token>) -> Vec<u8> {
-        let event = Event { name: name.to_string(), inputs: params.into_iter().map(|(name, kind, indexed)| EventParam { name: name.to_string(), kind, indexed }).collect(), anonymous: false };
+    pub fn to_log_entry_data(
+        name: &str,
+        params: EthEventParams,
+        locker_address: EthAddress,
+        indexes: Vec<Vec<u8>>,
+        values: Vec<Token>,
+    ) -> Vec<u8> {
+        let event = Event {
+            name: name.to_string(),
+            inputs: params
+                .into_iter()
+                .map(|(name, kind, indexed)| EventParam {
+                    name: name.to_string(),
+                    kind,
+                    indexed,
+                })
+                .collect(),
+            anonymous: false,
+        };
         let params: Vec<ParamType> = event.inputs.iter().map(|p| p.kind.clone()).collect();
         let topics = indexes.into_iter().map(|value| H256::from(value)).collect();
         let log_entry = LogEntry {
             address: locker_address.into(),
-            topics: vec![vec![
-                long_signature(&event.name, &params).0.into()], topics].concat(),
+            topics: vec![vec![long_signature(&event.name, &params).0.into()], topics].concat(),
             data: ethabi::encode(&values),
         };
         rlp::encode(&log_entry)
@@ -93,7 +119,11 @@ fn long_signature(name: &str, params: &[ParamType]) -> Hash {
 }
 
 fn fill_signature(name: &str, params: &[ParamType], result: &mut [u8]) {
-    let types = params.iter().map(Writer::write).collect::<Vec<String>>().join(",");
+    let types = params
+        .iter()
+        .map(Writer::write)
+        .collect::<Vec<String>>()
+        .join(",");
 
     let data: Vec<u8> = From::from(format!("{}({})", name, types).as_str());
 
