f897ac3fec1f5957c938ec408f5fae2c4802f13e
Marcelo Fornet <mfornet94@gmail.com>
2020-11-12 01:32:26 -0500

    Fuzz eth events

--- bridge-token-factory/tests/event_serialization.rs
+++ bridge-token-factory/tests/event_serialization.rs
@@ -1,3 +1,6 @@
+use bridge_token_factory::EthUnlockedEvent;
+use rand::prelude::ThreadRng;
+use rand::Rng;
 use std::fs;
 
 use bridge_token_factory::EthLockedEvent;
@@ -27,5 +30,53 @@ fn real_data_locked_event_serialization() {
 
         let re_serialize = locked_event.to_log_entry_data();
         assert_eq!(re_serialize, serialized);
+    }
+}
+
+fn rand_string(rng: &mut ThreadRng) -> String {
+    (0..rng.gen::<u8>())
+        .map(|_| (0x20u8 + (rng.gen::<f32>() * 96.0) as u8) as char)
+        .collect()
+}
+
+fn generate_random_eth_locked_event(rng: &mut ThreadRng) -> EthLockedEvent {
+    EthLockedEvent {
+        locker_address: rng.gen::<[u8; 20]>(),
+        token: hex::encode(rng.gen::<[u8; 20]>()),
+        sender: hex::encode(rng.gen::<[u8; 20]>()),
+        amount: rng.gen::<u128>(),
+        recipient: rand_string(rng),
+    }
+}
+
+fn generate_random_eth_unlocked_event(rng: &mut ThreadRng) -> EthUnlockedEvent {
+    EthUnlockedEvent {
+        locker_address: rng.gen::<[u8; 20]>(),
+        token: hex::encode(rng.gen::<[u8; 20]>()),
+        sender: hex::encode(rng.gen::<[u8; 20]>()),
+        amount: rng.gen::<u128>(),
+        recipient: rand_string(rng),
+    }
+}
+
+#[test]
+fn fuzzing_eth_locked() {
+    let mut rng = rand::thread_rng();
+    for _ in 0..1000 {
+        let event = generate_random_eth_locked_event(&mut rng);
+        let serialized = event.to_log_entry_data();
+        let deserialized = EthLockedEvent::from_log_entry_data(serialized.as_ref());
+        assert_eq!(event, deserialized);
+    }
+}
+
+#[test]
+fn fuzzing_eth_unlocked() {
+    let mut rng = rand::thread_rng();
+    for _ in 0..1000 {
+        let event = generate_random_eth_unlocked_event(&mut rng);
+        let serialized = event.to_log_entry_data();
+        let deserialized = EthUnlockedEvent::from_log_entry_data(serialized.as_ref());
+        assert_eq!(event, deserialized);
     }
 }
