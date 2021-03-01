31bcc5bad0641bf632ec8ec7ce979a41a0ed6f9b
Kirill <kirillinvade@gmail.com>
2021-03-01 22:11:14 +0200

    ERC20Connector: add replay protection. Refactoring (#35)
    * Add replay protection after the possible upgrade by providing the
    `minBlockAcceptanceHeight` constraint for the proofs acceptance.
    * Minor improvements.
    * Refactoring.

--- erc20-connector/test/UpdateProver.js
+++ erc20-connector/test/UpdateProver.js
@@ -5,16 +5,16 @@ const { serialize } = require('rainbow-bridge-lib/rainbow/borsh.js');
 const { borshifyOutcomeProof } = require('rainbow-bridge-lib/rainbow/borshify-proof.js');
 
 const ERC20Locker = artifacts.require('ERC20Locker');
-const ERC20Locker = artifacts.require('ERC20LockerAdmin');
 const NearProverMock = artifacts.require('test/NearProverMock');
 const TToken = artifacts.require('test/TToken');
 
 contract('UpdateProver', function ([admin]) {
     beforeEach(async function () {
+        let minBlockAcceptanceHeight = 0;
         this.token = await TToken.new();
         this.prover = await NearProverMock.new();
-        this.locker = await ERC20Locker.new(Buffer.from('nearfuntoken', 'utf-8'), this.prover.address, admin);
-        this.lockerAdmin = await ERC20Locker.new(Buffer.from('nearfuntoken', 'utf-8'), this.prover.address, admin);
+        this.locker = await ERC20Locker.new(Buffer.from('nearfuntoken', 'utf-8'), this.prover.address, minBlockAcceptanceHeight, admin);
+        this.lockerAdmin = await ERC20Locker.new(Buffer.from('nearfuntoken', 'utf-8'), this.prover.address, minBlockAcceptanceHeight, admin);
     });
 
     it('updateContract', async function () {
