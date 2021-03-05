21950d3036ae059bc92f1b418c285f2416bfdf07
Kirill <kirillinvade@gmail.com>
2021-03-05 08:50:17 +0200

    ERC20Locker: make AdminControlled. Add tests. (#36)
    * ERC20Locker: make AdminControlled. Add tests.
    
    * Add `AdminControlled` contract.
    * ERC20Locker: inherit from `AdminControlled`.
    * ERC20Locker: make `lockToken` and `unlockToken` pausable.
    * Add tests.
    
    * Apply suggestions from code review
    
    Co-authored-by: Marcelo Fornet <mfornet94@gmail.com>
    
    * ERC20Locker: add pauseFlags to ctor. Libs improvements.
    
    * Use AdminControlled from the rainbow-bridge repo;
    * ERC20Locker: add pauseFlags to constructor to be able to start the
      locker paused.
    * Fix tests.
    
    * Run tests in CI.
    
    * Run tests in CI: WIP.
    
    * Run test in CI. Stylystic fixes.
    
    * Run tests in CI: WIP.
    
    * Temporarily disable dependent on NEP21 test.
    
    Co-authored-by: Marcelo Fornet <mfornet94@gmail.com>

--- erc20-connector/dist/NearProverMock.full.sol
+++ erc20-connector/dist/NearProverMock.full.sol
@@ -4,7 +4,7 @@
 pragma solidity ^0.6;
 
 interface INearProver {
-    function proveOutcome(bytes calldata proofData, uint64 blockHeight) external view returns(bool);
+    function proveOutcome(bytes calldata proofData, uint64 blockHeight) external view returns (bool);
 }
 
 // File: contracts/test/NearProverMock.sol
