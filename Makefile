f1421b1fc29c4601e49b735b603530c6a34fa6af
Kirill <kirill@aurora.dev>
2021-09-17 00:25:15 +0300

    BridgeToken: add `icon` metadata field. Update metadata (#73)
    * BridgeToken: add `icon` metadata field.
    
    * Add icon to Tokens
    
    Token Contract now support icon field in their metadata.
    Logic to support trustless information is added.
    
    1. Add key to set metadata with power access, without requiring the Full Access Key
    2. Add icon field according to NEP148
    3. Interface for trustless bridge.
    
    * Update Makefile
    
    * delete no icon binary
    
    * Add interface in the factory to update metadata
    
    * Address comments + use .cargo
    
    * Fix CI
    
    * Update icon not supporte message
    
    * feat: token metadata connector (#71)
    
    * add ERC20 metadata connector contract
    
    * allow bridge token factory to update metadata
    
    * fix update NEP-141 metadata
    
    * fix tests
    
    * fix caller in bridge_token and add timestamp to the event
    
    * Finish metadata bridging primitives
    
    * Compile metadata emitter contract
    
    * Update amount of Gas
    
    Co-authored-by: Marcelo Fornet <mfornet94@gmail.com>
    
    * Address comments
    
    Co-authored-by: Marcelo Fornet <mfornet94@gmail.com>
    Co-authored-by: Ahmed Ali <ahmedaabdulwahed@gmail.com>

---
+++ Makefile
@@ -0,0 +1,42 @@
+CARGO = cargo
+
+all: res/bridge_token.wasm res/bridge_token_factory.wasm res/ERC20MetadataLogger.json
+
+res/bridge_token.wasm: $(shell find bridge-token/src -name "*.rs")
+	cd bridge-token && \
+	$(CARGO) build --target wasm32-unknown-unknown --release && \
+	cp target/wasm32-unknown-unknown/release/bridge_token.wasm ../res/ && \
+	ls -l ../res/bridge_token.wasm
+
+res/bridge_token_factory.wasm: export BRIDGE_TOKEN = $(realpath res/bridge_token.wasm)
+res/bridge_token_factory.wasm: res/bridge_token.wasm $(shell find bridge-token-factory/src -name "*.rs")
+	cd bridge-token-factory && \
+	$(CARGO) build --target wasm32-unknown-unknown --release && \
+	cp target/wasm32-unknown-unknown/release/bridge_token_factory.wasm ../res/ && \
+	ls -l ../res/bridge_token_factory.wasm
+
+res/ERC20MetadataLogger.json: metadata-connector/contracts/ERC20MetadataLogger.sol
+	cd metadata-connector && \
+	yarn && \
+	yarn compile && \
+	cp artifacts/contracts/ERC20MetadataLogger.sol/ERC20MetadataLogger.json ../res/ && \
+	ls -l ../res/ERC20MetadataLogger.json
+
+# <--
+# For testing purposes only (can be removed at any moment)
+no_icon: res/bridge_token_no_icon.wasm res/bridge_token_factory_no_icon.wasm
+
+res/bridge_token_no_icon.wasm: $(shell find bridge-token/src -name "*.rs")
+	cd bridge-token && \
+	BRIDGE_TOKEN=$(realpath ../res/bridge_token.wasm) $(CARGO) build --target wasm32-unknown-unknown --no-default-features --release && \
+	cp target/wasm32-unknown-unknown/release/bridge_token.wasm ../res/bridge_token_no_icon.wasm && \
+	ls -l ../res/bridge_token_no_icon.wasm
+
+res/bridge_token_factory_no_icon.wasm: export BRIDGE_TOKEN=$(realpath res/bridge_token_no_icon.wasm)
+res/bridge_token_factory_no_icon.wasm: res/bridge_token_no_icon.wasm $(shell find bridge-token-factory/src -name "*.rs")
+	cd bridge-token-factory && \
+    $(CARGO) build --target wasm32-unknown-unknown --release && \
+	cp target/wasm32-unknown-unknown/release/bridge_token_factory.wasm ../res/bridge_token_factory_no_icon.wasm && \
+	ls -l ../res/bridge_token_factory_no_icon.wasm
+# -->
+
--- bridge-token/src/lib.rs
+++ bridge-token/src/lib.rs
@@ -6,8 +6,8 @@ use near_contract_standards::fungible_token::FungibleToken;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::{Base64VecU8, ValidAccountId, U128};
 use near_sdk::{
-    assert_one_yocto, assert_self, env, ext_contract, near_bindgen, AccountId, Balance, Gas,
-    PanicOnDefault, Promise, PromiseOrValue, StorageUsage,
+    assert_one_yocto, env, ext_contract, near_bindgen, AccountId, Balance, Gas, PanicOnDefault,
+    Promise, PromiseOrValue, StorageUsage,
 };
 use std::convert::TryInto;
 
@@ -29,6 +29,8 @@ pub struct BridgeToken {
     reference_hash: Base64VecU8,
     decimals: u8,
     paused: Mask,
+    #[cfg(feature = "migrate_icon")]
+    icon: Option<String>,
 }
 
 const PAUSE_WITHDRAW: Mask = 1 << 0;
@@ -57,6 +59,8 @@ impl BridgeToken {
             reference_hash: Base64VecU8(vec![]),
             decimals: 0,
             paused: Mask::default(),
+            #[cfg(feature = "migrate_icon")]
+            icon: None,
         }
     }
 
@@ -67,14 +71,22 @@ impl BridgeToken {
         reference: Option<String>,
         reference_hash: Option<Base64VecU8>,
         decimals: Option<u8>,
+        icon: Option<String>,
     ) {
         // Only owner can change the metadata
-        assert_self();
+        assert!(self.controller_or_self());
+
         name.map(|name| self.name = name);
         symbol.map(|symbol| self.symbol = symbol);
         reference.map(|reference| self.reference = reference);
         reference_hash.map(|reference_hash| self.reference_hash = reference_hash);
         decimals.map(|decimals| self.decimals = decimals);
+        #[cfg(feature = "migrate_icon")]
+        icon.map(|icon| self.icon = Some(icon));
+        #[cfg(not(feature = "migrate_icon"))]
+        icon.map(|_| {
+            env::log("Icon was provided, but it's not supported for the token".as_bytes())
+        });
     }
 
     #[payable]
@@ -111,6 +123,12 @@ impl BridgeToken {
     pub fn account_storage_usage(&self) -> StorageUsage {
         self.token.account_storage_usage
     }
+
+    /// Return true if the caller is either controller or self
+    pub fn controller_or_self(&self) -> bool {
+        let caller = env::predecessor_account_id();
+        caller == self.controller || caller == env::current_account_id()
+    }
 }
 
 near_contract_standards::impl_fungible_token_core!(BridgeToken, token);
@@ -123,9 +141,10 @@ impl FungibleTokenMetadataProvider for BridgeToken {
             spec: FT_METADATA_SPEC.to_string(),
             name: self.name.clone(),
             symbol: self.symbol.clone(),
-            icon: Some(
-                "https://near.org/wp-content/themes/near-19/assets/img/brand-icon.png".to_string(),
-            ),
+            #[cfg(feature = "migrate_icon")]
+            icon: self.icon.clone(),
+            #[cfg(not(feature = "migrate_icon"))]
+            icon: None,
             reference: Some(self.reference.clone()),
             reference_hash: Some(self.reference_hash.clone()),
             decimals: self.decimals,
@@ -134,3 +153,50 @@ impl FungibleTokenMetadataProvider for BridgeToken {
 }
 
 admin_controlled::impl_admin_controlled!(BridgeToken, paused);
+
+// Migration
+
+#[cfg(feature = "migrate_icon")]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct BridgeTokenV0 {
+    controller: AccountId,
+    token: FungibleToken,
+    name: String,
+    symbol: String,
+    reference: String,
+    reference_hash: Base64VecU8,
+    decimals: u8,
+    paused: Mask,
+}
+
+#[cfg(feature = "migrate_icon")]
+impl From<BridgeTokenV0> for BridgeToken {
+    fn from(obj: BridgeTokenV0) -> Self {
+        Self {
+            controller: obj.controller,
+            token: obj.token,
+            name: obj.name,
+            symbol: obj.symbol,
+            reference: obj.reference,
+            reference_hash: obj.reference_hash,
+            decimals: obj.decimals,
+            paused: obj.paused,
+            icon: None,
+        }
+    }
+}
+
+#[cfg(feature = "migrate_icon")]
+#[near_bindgen]
+impl BridgeToken {
+    /// Adding icon as suggested here: https://nomicon.io/Standards/FungibleToken/Metadata.html
+    /// This function can only be called from the factory or from the contract itself.
+    #[init(ignore_state)]
+    pub fn migrate_nep_148_add_icon() -> Self {
+        let old_state: BridgeTokenV0 = env::state_read()
+            .expect("State is not compatible with BridgeTokenV0. Migration has not been applied.");
+        let new_state: BridgeToken = old_state.into();
+        assert!(new_state.controller_or_self());
+        new_state
+    }
+}
