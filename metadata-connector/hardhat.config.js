241d6a9a7450f43372639440e2d2c4e69c6b34c2
Marcelo Fornet <mfornet94@gmail.com>
2021-09-16 00:00:30 -0400

    Compile metadata emitter contract

--- metadata-connector/hardhat.config.js
+++ metadata-connector/hardhat.config.js
@@ -5,20 +5,20 @@ const AURORA_PRIVATE_KEY = process.env.AURORA_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.5",
-  networks: {
-    aurora: {
-      url: 'https://testnet.aurora.dev',
-      accounts: [`0x${AURORA_PRIVATE_KEY}`],
-      chainId: 1313161555,
-      gasPrice: 120 * 1000000000
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x${AURORA_PRIVATE_KEY}`],
-      chainId: 3,
-      live: true,
-      gasPrice: 50000000000,
-      gasMultiplier: 2,
-    },
-  }
+  // networks: {
+  //   aurora: {
+  //     url: 'https://testnet.aurora.dev',
+  //     accounts: [`0x${AURORA_PRIVATE_KEY}`],
+  //     // chainId: 1313161555,
+  //     // gasPrice: 120 * 1000000000
+  //   },
+  //   ropsten: {
+  //     url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+  //     accounts: [`0x${AURORA_PRIVATE_KEY}`],
+  //     chainId: 3,
+  //     live: true,
+  //     gasPrice: 50000000000,
+  //     gasMultiplier: 2,
+  //   },
+  // }
 };
