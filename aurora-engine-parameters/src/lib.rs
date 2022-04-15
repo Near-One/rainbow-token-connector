25c15974c34179a51e8624f749843b725d749c9a
karim-en <karim@aurora.dev>
2022-04-15 12:06:10 +0100

    Update engiine parametes crate

--- aurora-engine-parameters/src/lib.rs
+++ aurora-engine-parameters/src/lib.rs
@@ -1,4 +1,4 @@
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use borsh::{self, BorshDeserialize, BorshSerialize};
 
 pub type EthAddress = [u8; 20];
 pub type RawU256 = [u8; 32];
@@ -18,11 +18,26 @@ pub enum TransactionStatus {
 
 /// Borsh-encoded parameters for the engine `call` function.
 #[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq, Eq, Clone)]
-pub struct FunctionCallArgs {
+pub struct FunctionCallArgsV2 {
     pub contract: EthAddress,
     /// Wei compatible Borsh-encoded value field to attach an ETH balance to the transaction
     pub value: WeiU256,
     pub input: Vec<u8>,
+}
+
+/// Legacy Borsh-encoded parameters for the engine `call` function, to provide backward type compatibility
+#[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq, Eq, Clone)]
+pub struct FunctionCallArgsV1 {
+    pub contract: EthAddress,
+    pub input: Vec<u8>,
+}
+
+/// Deserialized values from bytes to current or legacy Borsh-encoded parameters
+/// for passing to the engine `call` function, and to provide backward type compatibility
+#[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq, Eq, Clone)]
+pub enum CallArgs {
+    V2(FunctionCallArgsV2),
+    V1(FunctionCallArgsV1),
 }
 
 /// Borsh-encoded parameters for the `view` function.
