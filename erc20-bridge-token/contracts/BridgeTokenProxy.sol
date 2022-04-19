428b70f367ad72e2258945506c6ac1751648740d
Shockedshodan <46688871+Shockedshodan@users.noreply.github.com>
2022-04-19 16:59:13 +0100

    add proxy contract

---
+++ erc20-bridge-token/contracts/BridgeTokenProxy.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+import "@openzeppelin/contracts/proxy/Proxy.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+
+contract BridgeTokenProxy is ERC1967Proxy, AccessControlUpgradeable {
+    constructor(address _logic, bytes memory _data)
+        ERC1967Proxy(_logic, _data)
+    {
+        __AccessControl_init();
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function upgradeTo(address implementation)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+        super._upgradeTo(implementation);
+    }
+}
