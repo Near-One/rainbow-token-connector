d172ba5e9b5b8eb02a240c394172378db299ad31
Shockedshodan <46688871+Shockedshodan@users.noreply.github.com>
2022-04-21 21:16:41 +0100

    Updated locker

--- erc20-bridge-token/contracts/ERC20Locker.sol
+++ erc20-bridge-token/contracts/ERC20Locker.sol
@@ -1,15 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "rainbow-bridge-sol/nearprover/contracts/INearProver.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "rainbow-bridge-sol/nearbridge/contracts/AdminControlled.sol";
 import "rainbow-bridge-sol/nearprover/contracts/ProofDecoder.sol";
-import "rainbow-bridge-sol/nearbridge/contracts/NearDecoder.sol";
 import "rainbow-bridge-sol/nearbridge/contracts/Borsh.sol";
-
 import "./Locker.sol";
 
-contract ERC20Locker is Locker {
+contract ERC20Locker is Locker, AdminControlled {
+    using SafeMath for uint256;
     using SafeERC20 for IERC20;
     using Borsh for Borsh.Data;
 
@@ -32,19 +33,36 @@ contract ERC20Locker is Locker {
         address recipient;
     }
 
-    function lockToken(address ethToken, uint256 amount, string memory accountId) public {
+    uint constant UNPAUSED_ALL = 0;
+    uint constant PAUSED_LOCK = 1 << 0;
+    uint constant PAUSED_UNLOCK = 1 << 1;
+
+    // ERC20Locker is linked to the bridge token factory on NEAR side.
+    // It also links to the prover that it uses to unlock the tokens.
+    constructor(bytes memory _nearTokenFactory,
+                INearProver _prover,
+                uint64 _minBlockAcceptanceHeight,
+                address _admin,
+                uint _pausedFlags)
+        AdminControlled(_admin, _pausedFlags)
+        Locker(_nearTokenFactory, _prover, _minBlockAcceptanceHeight)
+    {
+    }
+
+    function lockToken(address ethToken, uint256 amount, string memory accountId)
+        public
+        pausable (PAUSED_LOCK)
+    {
+        require(IERC20(ethToken).balanceOf(address(this)).add(amount) <= ((uint256(1) << 128) - 1), "Maximum tokens locked exceeded (< 2^128 - 1)");
         IERC20(ethToken).safeTransferFrom(msg.sender, address(this), amount);
         emit Locked(address(ethToken), msg.sender, amount, accountId);
     }
 
-    function burnResult(bytes memory proofData, uint64 proofBlockHeight) public returns(address) {
-        ProofDecoder.ExecutionStatus memory status = _parseProof(proofData, proofBlockHeight);
-        BurnResult memory result = _decodeBurnResult(status.successValue);
-        return result.token;
-    }
-
-    function unlockToken(bytes memory proofData, uint64 proofBlockHeight) public {
-        ProofDecoder.ExecutionStatus memory status = _parseProof(proofData, proofBlockHeight);
+    function unlockToken(bytes memory proofData, uint64 proofBlockHeight)
+        public
+        pausable (PAUSED_UNLOCK)
+    {
+        ProofDecoder.ExecutionStatus memory status = _parseAndConsumeProof(proofData, proofBlockHeight);
         BurnResult memory result = _decodeBurnResult(status.successValue);
         IERC20(result.token).safeTransfer(result.recipient, result.amount);
         emit Unlocked(result.amount, result.recipient);
@@ -52,11 +70,26 @@ contract ERC20Locker is Locker {
 
     function _decodeBurnResult(bytes memory data) internal pure returns(BurnResult memory result) {
         Borsh.Data memory borshData = Borsh.from(data);
+        uint8 flag = borshData.decodeU8();
+        require(flag == 0, "ERR_NOT_WITHDRAW_RESULT");
         result.amount = borshData.decodeU128();
         bytes20 token = borshData.decodeBytes20();
         result.token = address(uint160(token));
         bytes20 recipient = borshData.decodeBytes20();
         result.recipient = address(uint160(recipient));
         borshData.done();
+    }
+
+    // tokenFallback implements the ContractReceiver interface from ERC223-token-standard.
+    // This allows to support ERC223 tokens with no extra cost.
+    // The function always passes: we don't need to make any decision and the contract always
+    // accept token transfers transfer.
+    function tokenFallback(address _from, uint _value, bytes memory _data) public pure {}
+
+    function adminTransfer(IERC20 token, address destination, uint amount)
+        public
+        onlyAdmin
+    {
+        token.safeTransfer(destination, amount);
     }
 }
--- erc20-bridge-token/contracts/Locker.sol
+++ erc20-bridge-token/contracts/Locker.sol
@@ -1,38 +1,60 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8;
 
 import "rainbow-bridge-sol/nearprover/contracts/INearProver.sol";
 import "rainbow-bridge-sol/nearprover/contracts/ProofDecoder.sol";
-import "rainbow-bridge-sol/nearbridge/contracts/NearDecoder.sol";
 import "rainbow-bridge-sol/nearbridge/contracts/Borsh.sol";
-
 contract Locker {
     using Borsh for Borsh.Data;
     using ProofDecoder for Borsh.Data;
-    using NearDecoder for Borsh.Data;
 
-    INearProver public prover_;
-    bytes public nearTokenFactory_;
+    INearProver public prover;
+    bytes public nearTokenFactory;
+
+    /// Proofs from blocks that are below the acceptance height will be rejected.
+    // If `minBlockAcceptanceHeight` value is zero - proofs from block with any height are accepted.
+    uint64 public minBlockAcceptanceHeight;
 
     // OutcomeReciptId -> Used
-    mapping(bytes32 => bool) public usedEvents_;
+    mapping(bytes32 => bool) public usedProofs;
 
-    function _parseProof(bytes memory proofData, uint64 proofBlockHeight) internal returns(ProofDecoder.ExecutionStatus memory result) {
-        require(prover_.proveOutcome(proofData, proofBlockHeight), "Proof should be valid");
+    constructor(bytes memory _nearTokenFactory, INearProver _prover, uint64 _minBlockAcceptanceHeight) {
+        require(_nearTokenFactory.length > 0, "Invalid Near Token Factory address");
+        require(address(_prover) != address(0), "Invalid Near prover address");
+
+        nearTokenFactory = _nearTokenFactory;
+        prover = _prover;
+        minBlockAcceptanceHeight = _minBlockAcceptanceHeight;
+    }
+
+    /// Parses the provided proof and consumes it if it's not already used.
+    /// The consumed event cannot be reused for future calls.
+    function _parseAndConsumeProof(bytes memory proofData, uint64 proofBlockHeight)
+        internal
+        returns (ProofDecoder.ExecutionStatus memory result)
+    {
+        require(prover.proveOutcome(proofData, proofBlockHeight), "Proof should be valid");
 
         // Unpack the proof and extract the execution outcome.
         Borsh.Data memory borshData = Borsh.from(proofData);
         ProofDecoder.FullOutcomeProof memory fullOutcomeProof = borshData.decodeFullOutcomeProof();
         borshData.done();
 
-        bytes32 receiptId = fullOutcomeProof.outcome_proof.outcome_with_id.outcome.receipt_ids[0];
-        require(!usedEvents_[receiptId], "The burn event cannot be reused");
-        usedEvents_[receiptId] = true;
+        require(
+            fullOutcomeProof.block_header_lite.inner_lite.height >= minBlockAcceptanceHeight,
+            "Proof is from the ancient block"
+        );
 
-        require(keccak256(fullOutcomeProof.outcome_proof.outcome_with_id.outcome.executor_id) == keccak256(nearTokenFactory_),
-        "Can only unlock tokens from the linked mintable fungible token on Near blockchain.");
+        bytes32 receiptId = fullOutcomeProof.outcome_proof.outcome_with_id.outcome.receipt_ids[0];
+        require(!usedProofs[receiptId], "The burn event proof cannot be reused");
+        usedProofs[receiptId] = true;
+
+        require(keccak256(fullOutcomeProof.outcome_proof.outcome_with_id.outcome.executor_id)
+                == keccak256(nearTokenFactory),
+                "Can only unlock tokens from the linked proof producer on Near blockchain");
 
         result = fullOutcomeProof.outcome_proof.outcome_with_id.outcome.status;
-        require(!result.failed, "Cannot use failed execution outcome for unlocking the tokens.");
-        require(!result.unknown, "Cannot use unknown execution outcome for unlocking the tokens.");
+        require(!result.failed, "Cannot use failed execution outcome for unlocking the tokens");
+        require(!result.unknown, "Cannot use unknown execution outcome for unlocking the tokens");
     }
 }
