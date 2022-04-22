cd857aed69f04bd0c0b16c49a7be33b21644a563
Shockedshodan <46688871+Shockedshodan@users.noreply.github.com>
2022-04-22 17:56:15 +0100

    safe upgradeability, provermock

--- erc20-bridge-token/contracts/BridgeTokenFactory.sol
+++ erc20-bridge-token/contracts/BridgeTokenFactory.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.8;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
@@ -18,8 +18,8 @@ import "./BridgeTokenProxy.sol";
 contract BridgeTokenFactory is Locker, AccessControlUpgradeable, PausableUpgradeable{
 
     using Borsh for Borsh.Data;
-    using SafeMath for uint256;
-    using SafeERC20 for IERC20;
+    using SafeMathUpgradeable for uint256;
+    using SafeERC20Upgradeable for IERC20Upgradeable;
     
     mapping(address => string) private _ethToNearToken;
     mapping(string => address) private _nearToEthToken;
