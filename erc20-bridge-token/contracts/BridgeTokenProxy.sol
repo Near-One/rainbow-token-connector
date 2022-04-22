61921c1ce08031bc5cfd5045d3695b9f0310af1d
Shockedshodan <46688871+Shockedshodan@users.noreply.github.com>
2022-04-22 19:10:46 +0100

    fixed proxy

--- erc20-bridge-token/contracts/BridgeTokenProxy.sol
+++ erc20-bridge-token/contracts/BridgeTokenProxy.sol
@@ -1,13 +1,12 @@
 pragma solidity ^0.8;
 import "@openzeppelin/contracts/proxy/Proxy.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract BridgeTokenProxy is ERC1967Proxy, AccessControlUpgradeable {
+contract BridgeTokenProxy is ERC1967Proxy, AccessControl {
     constructor(address _logic, bytes memory _data)
-        ERC1967Proxy(_logic, _data)
+        ERC1967Proxy(_logic, _data) AccessControl()
     {
-        __AccessControl_init();
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
