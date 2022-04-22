a6bdeeafad1249a9cea9142a412af6efea491f92
Shockedshodan <46688871+Shockedshodan@users.noreply.github.com>
2022-04-22 19:49:35 +0100

    add coverage plugin

--- erc20-bridge-token/hardhat.config.js
+++ erc20-bridge-token/hardhat.config.js
@@ -1,6 +1,9 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+ require('@nomiclabs/hardhat-ethers');
+ require('@openzeppelin/hardhat-upgrades');
+ require("solidity-coverage");
 
 module.exports = {
   paths: {
