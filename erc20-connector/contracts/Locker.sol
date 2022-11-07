5f20c2f3c5797ffcff2dc804e68208a5cfa8c1c5
Kirill <kirill@aurora.dev>
2022-11-07 03:30:40 +0000

    Locker: lock pragma to Solidity v0.8.11

--- erc20-connector/contracts/Locker.sol
+++ erc20-connector/contracts/Locker.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8;
+pragma solidity ^0.8.11;
 
 import "rainbow-bridge-sol/nearprover/contracts/INearProver.sol";
 import "rainbow-bridge-sol/nearprover/contracts/ProofDecoder.sol";
