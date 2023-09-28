12caaeafaba23c54759554c5f498707283b36a0f
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2023-09-28 19:21:40 +0300

    scripts for interactions

--- silo-to-silo/hardhat.config.js
+++ silo-to-silo/hardhat.config.js
@@ -16,6 +16,7 @@ task("deploy", "Deploy silo to silo proxy contract")
       signer,
       wNearAddress: config.wNearAddress,
       siloAccountId: config.siloAccountId,
+      nativeTokenAccountId: config.nativeTokenAccountId,
       auroraSdkAddress: config.auroraSdkAddress,
       auroraUtilsAddress: config.auroraUtilsAddress,
     });
@@ -38,6 +39,134 @@ task("upgrade", "Upgrade silo to silo proxy contract")
     });
   });
 
+task('register_token', 'Registers a binding of "nearTokenAccountId:auroraTokenAddress" in "SiloToSilo" contract.')
+    .addParam("silo", "Config file name without extension")
+    .addParam("proxy", "Current proxy address of the SiloToSilo contract")
+    .addParam('auroraTokenAddress', "Token address on Aurora")
+    .setAction(async taskArgs => {
+        const { registerToken } = require('./utils/scripts.js');
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await registerToken(signer, config, taskArgs.proxy, taskArgs.auroraTokenAddress);
+    });
+
+
+task('get_token_near_account_id', 'Get Token Near Account Id from SiloToSilo')
+    .addParam("silo", "Config file name without extension")
+    .addParam("proxy", "Current proxy address of the SiloToSilo contract")
+    .addParam('auroraTokenAddress', "Token address on Aurora")
+    .setAction(async taskArgs => {
+        const { getTokenNearAccountId } = require('./utils/scripts.js');
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await getTokenNearAccountId(signer, config, taskArgs.proxy, taskArgs.auroraTokenAddress);
+    });
+
+task('storage_deposit', 'Puts a storage deposit in "nearTokenAccountId" for the "SiloToSilo" implicit NEAR Account ID.')
+    .addParam("silo", "Config file name without extension")
+    .addParam("proxy", "Current proxy address of the SiloToSilo contract")
+    .addParam('auroraTokenAddress', "Token address on Aurora")
+    .setAction(async taskArgs => {
+        const { storageDeposit } = require('./utils/scripts.js');
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await storageDeposit(signer, config, taskArgs.proxy, taskArgs.auroraTokenAddress);
+    });
+
+task('is_storage_registered', 'Check is storage registered for token')
+    .addParam("silo", "Config file name without extension")
+    .addParam("proxy", "Current proxy address of the SiloToSilo contract")
+    .addParam('auroraTokenAddress', "Token address on Aurora")
+    .setAction(async taskArgs => {
+        const { isStorageRegistered } = require('./utils/scripts.js');
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await isStorageRegistered(signer, config, taskArgs.proxy, taskArgs.auroraTokenAddress);
+    });
+
+task('safe_ft_transfer_call_to_near', 'Init ft transfer call on Near')
+    .addParam("silo", "Config file name without extension")
+    .addParam("proxy", "Current proxy address of the SiloToSilo contract")
+    .addParam("receiverId", "Receiver Id")
+    .addParam("amount", "Transferred tokens amount")
+    .addParam('msg', "Msg for ft_transfer_call")
+    .addParam('auroraTokenAddress', "Token address on Aurora")
+    .setAction(async taskArgs => {
+        const { safeFtTransferCallToNear } = require('./utils/scripts.js');
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        if (taskArgs.msg == "") {
+            taskArgs.msg = signer.address.substring(2);
+        }
+
+        await safeFtTransferCallToNear(signer, config, taskArgs.proxy, taskArgs.auroraTokenAddress, taskArgs.receiverId, taskArgs.amount, taskArgs.msg);
+    });
+
+task('recipient_storage_deposit', "Storage Deposit for Recipient")
+    .addParam("silo", "Config file name without extension")
+    .addParam("proxy", "Current proxy address of the SiloToSilo contract")
+    .addParam("receiverId", "Receiver Id")
+    .addParam('auroraTokenAddress', "Token address on Aurora")
+    .setAction(async taskArgs => {
+        const { recipientStorageDeposit } = require('./utils/scripts.js');
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await recipientStorageDeposit(signer, config, taskArgs.proxy, taskArgs.auroraTokenAddress, taskArgs.receiverId);
+    });
+
+task('is_recipient_storage_registered', "Check if Storage Deposited for Recipient")
+    .addParam("silo", "Config file name without extension")
+    .addParam("proxy", "Current proxy address of the SiloToSilo contract")
+    .addParam("receiverId", "Receiver Id")
+    .addParam('auroraTokenAddress', "Token address on Aurora")
+    .setAction(async taskArgs => {
+        const { isRecipientStorageRegistered } = require('./utils/scripts.js');
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await isRecipientStorageRegistered(signer, config, taskArgs.proxy, taskArgs.auroraTokenAddress, taskArgs.receiverId);
+    });
+
+task('get_user_balance', "Get user balance")
+    .addParam("silo", "Config file name without extension")
+    .addParam("proxy", "Current proxy address of the SiloToSilo contract")
+    .addParam('auroraTokenAddress', "Token address on Aurora")
+    .setAction(async taskArgs => {
+        const { getUserBalance } = require('./utils/scripts.js');
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await getUserBalance(signer, config, taskArgs.proxy, taskArgs.auroraTokenAddress);
+    });
+
+task('withdraw', "Withdraw tokens")
+    .addParam("silo", "Config file name without extension")
+    .addParam("proxy", "Current proxy address of the SiloToSilo contract")
+    .addParam('auroraTokenAddress', "Token address on Aurora")
+    .setAction(async taskArgs => {
+        const { withdraw } = require('./utils/scripts.js');
+        const [signer] = await hre.ethers.getSigners();
+        const config = require(`./configs/${taskArgs.silo}.json`);
+
+        await withdraw(signer, config, taskArgs.proxy, taskArgs.auroraTokenAddress);
+    });
+
+task("deploy-sdk", "Deploy aurora sdk").setAction(async (_, hre) => {
+    const { deploySDK } = require("./utils/scripts.js");
+    const [signer] = await hre.ethers.getSigners();
+
+    await hre.run("compile");
+    await deploySDK({
+        signer,
+    });
+});
+
 module.exports = {
   solidity: {
     version: "0.8.21",
@@ -49,6 +178,11 @@ module.exports = {
     },
   },
   networks: {
+    mainnet_aurora: {
+       url: 'https://mainnet.aurora.dev',
+       accounts: [`0x${AURORA_PRIVATE_KEY}`],
+       chainId: 1313161554,
+    },
     testnet_aurora: {
       url: "https://testnet.aurora.dev",
       accounts: [`0x${AURORA_PRIVATE_KEY}`],
@@ -58,6 +192,10 @@ module.exports = {
       url: "https://develop.rpc.testnet.aurora.dev:8545",
       accounts: [`0x${AURORA_PRIVATE_KEY}`],
     },
+    enpower_silo_aurora: {
+        url: "https://enpower.aurora.dev",
+        accounts: [`0x${AURORA_PRIVATE_KEY}`],
+    }
   },
   mocha: {
     timeout: 100000000,
--- silo-to-silo/utils/scripts.js
+++ silo-to-silo/utils/scripts.js
@@ -5,6 +5,7 @@ async function deploy({
   signer,
   wNearAddress,
   siloAccountId,
+  nativeTokenAccountId,
   auroraSdkAddress,
   auroraUtilsAddress,
 }) {
@@ -25,7 +26,7 @@ async function deploy({
 
   let proxy = await upgrades.deployProxy(
     SiloToSiloContract,
-    [wNearAddress, siloAccountId],
+    [wNearAddress, siloAccountId, nativeTokenAccountId],
     {
       initializer: "initialize",
       unsafeAllowLinkedLibraries: true,
@@ -41,6 +42,26 @@ async function deploy({
   );
 }
 
+async function deploySDK({ signer }) {
+  let utilsLib = await ethers.deployContract("Utils", { signer });
+  await utilsLib.waitForDeployment();
+  console.log("Utils lib deployed to: ", await utilsLib.getAddress());
+
+  let codecLib = await ethers.deployContract("Codec", { signer });
+  await codecLib.waitForDeployment();
+  console.log("Codec lib deployed to: ", await codecLib.getAddress());
+
+  const sdkLib = await ethers.deployContract("AuroraSdk", {
+    signer,
+    libraries: {
+      Utils: await utilsLib.getAddress(),
+      Codec: await codecLib.getAddress(),
+    },
+  });
+  await sdkLib.waitForDeployment();
+  console.log("SDK lib deployed to: ", await sdkLib.getAddress());
+}
+
 async function upgrade({
   signer,
   proxyAddress,
@@ -49,22 +70,22 @@ async function upgrade({
 }) {
   console.log("Upgrading contracts with the account:", signer.address);
   console.log(
-    "Account balance:",
-    (await signer.provider.getBalance(signer.address)).toString(),
+      "Account balance:",
+      (await signer.provider.getBalance(signer.address)).toString(),
   );
 
   const SiloToSiloContract = (
-    await ethers.getContractFactory("SiloToSilo", {
-      libraries: {
-        AuroraSdk: auroraSdkAddress,
-        Utils: auroraUtilsAddress,
-      },
-    })
+      await ethers.getContractFactory("SiloToSilo", {
+        libraries: {
+          AuroraSdk: auroraSdkAddress,
+          Utils: auroraUtilsAddress,
+        },
+      })
   ).connect(signer);
 
   console.log(
-    "Current implementation address:",
-    await upgrades.erc1967.getImplementationAddress(proxyAddress),
+      "Current implementation address:",
+      await upgrades.erc1967.getImplementationAddress(proxyAddress),
   );
   console.log("Upgrade SiloToSilo contract, proxy address", proxyAddress);
   const proxy = await upgrades.upgradeProxy(proxyAddress, SiloToSiloContract, {
@@ -74,10 +95,107 @@ async function upgrade({
   await proxy.waitForDeployment();
 
   console.log(
-    "SiloToSilo impl deployed to: ",
-    await upgrades.erc1967.getImplementationAddress(await proxy.getAddress()),
+      "SiloToSilo impl deployed to: ",
+      await upgrades.erc1967.getImplementationAddress(await proxy.getAddress()),
   );
+}
+
+async function registerToken(signer, config, proxy, auroraTokenAddress) {
+  const siloToSilo = await getSiloToSiloContract(signer, config, proxy);
+
+  const wnear = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", config.wNearAddress);
+  await wnear.approve(proxy, "2000000000000000000000000");
+
+  await siloToSilo.registerToken(auroraTokenAddress);
+}
+
+async function getTokenNearAccountId(signer, config, proxy, auroraTokenAddress) {
+  const siloToSilo = await getSiloToSiloContract(signer, config, proxy);
+  console.log("Near Account Id for Token: ", await siloToSilo.getTokenAccountId(auroraTokenAddress));
+}
+
+async function storageDeposit(signer, config, proxy, auroraTokenAddress) {
+  const siloToSilo = await getSiloToSiloContract(signer, config, proxy);
+
+  const wnear = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", config.wNearAddress);
+  await wnear.approve(proxy, "12500000000000000000000");
+
+  await siloToSilo.storageDeposit(auroraTokenAddress, "12500000000000000000000");
+}
+
+
+async function isStorageRegistered(signer, config, proxy, auroraTokenAddress) {
+  const siloToSilo = await getSiloToSiloContract(signer, config, proxy);
+
+  console.log("Is Storage Registered: ", await siloToSilo.isStorageRegistered(auroraTokenAddress));
+}
+
+async function safeFtTransferCallToNear(signer, config, proxy, auroraTokenAddress, receiverId, amount, msg) {
+  const siloToSilo = await getSiloToSiloContract(signer, config, proxy);
+
+  const wnear = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", config.wNearAddress);
+  await wnear.transfer(siloToSilo, 1);
+
+  const token = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", auroraTokenAddress);
+  await token.approve(proxy, amount);
+
+  await siloToSilo.safeFtTransferCallToNear(auroraTokenAddress, amount, receiverId, msg);
+}
+
+async function recipientStorageDeposit(signer, config, proxy, auroraTokenAddress, receiverId) {
+  const siloToSilo = await getSiloToSiloContract(signer, config, proxy);
+
+  const wnear = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", config.wNearAddress);
+  await wnear.approve(proxy, "12500000000000000000000");
+
+  await siloToSilo.recipientStorageDeposit(auroraTokenAddress, "12500000000000000000000", receiverId);
+}
+
+async function isRecipientStorageRegistered(signer, config, proxy, auroraTokenAddress, receiverId) {
+  const siloToSilo = await getSiloToSiloContract(signer, config, proxy);
+
+  console.log("Is Storage Registered: ", await siloToSilo.isRecipientStorageRegistered(auroraTokenAddress, receiverId));
+}
+
+async function getUserBalance(signer, config, proxy, auroraTokenAddress) {
+  const siloToSilo = await getSiloToSiloContract(signer, config, proxy);
+
+  console.log("User balance: ", await siloToSilo.getUserBalance(auroraTokenAddress, signer.address));
+}
+
+async function withdraw(signer, config, proxy, auroraTokenAddress) {
+  const siloToSilo = await getSiloToSiloContract(signer, config, proxy);
+
+  const wnear = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", config.wNearAddress);
+  await wnear.transfer(siloToSilo, 1);
+
+  await siloToSilo.withdraw(auroraTokenAddress);
+}
+
+async function getSiloToSiloContract(signer, config, proxy) {
+  console.log("Sending transaction with the account:", signer.address);
+
+  const SiloToSilo = await hre.ethers.getContractFactory("SiloToSilo", {
+    libraries: {
+      "AuroraSdk": config.auroraSdkAddress,
+      "Utils": config.auroraUtilsAddress
+    },
+  });
+
+  return SiloToSilo
+      .attach(proxy)
+      .connect(signer);
 }
 
 exports.deploy = deploy;
 exports.upgrade = upgrade;
+exports.registerToken = registerToken;
+exports.getTokenNearAccountId = getTokenNearAccountId;
+exports.storageDeposit = storageDeposit;
+exports.isStorageRegistered = isStorageRegistered;
+exports.safeFtTransferCallToNear = safeFtTransferCallToNear;
+exports.recipientStorageDeposit = recipientStorageDeposit;
+exports.isRecipientStorageRegistered = isRecipientStorageRegistered;
+exports.getUserBalance = getUserBalance;
+exports.withdraw = withdraw;
+exports.deploySDK = deploySDK;
